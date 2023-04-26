16868a0f4fff46cd7158fdf02b69d28c80abfc51
Rodrigo Souza <rodrigosouza2k@outlook.com>
2023-04-25 21:08:38 -0300

    01

--- desafio01.js
+++ desafio01.js
@@ -1,3 +1,17 @@
-let preco_combustivel = 3.50;
-let gasto_por_km;
-let distancia_em_km;
+/* Faça um programa para calcular o valor de uma viagem.
+
+Você terá 3 variáveis. Sendo elas:
+ 1 - Preço do combustível;
+ 2 - Gasto médio de combustível do carro por KM;
+ 3 - Distância em KM da viagem;
+
+Imprima no console o valor que será gasto para realizar esta viagem.
+*/
+const preco_combustivel = 5.48;
+const km_por_litros = 10;
+const distancia_em_km = 100;
+
+const consumoporlitro = distancia_em_km / km_por_litros;
+
+const valor_final = consumoporlitro * preco_combustivel;
+console.log(valor_final.toFixed(2))
