24f9e29afa442cf5e840dbb9a1664362dcc644c5
Rodrigo Souza <rodrigosouza2k@outlook.com>
2023-04-30 12:40:10 -0300

    01

--- condicionais.js
+++ condicionais.js
@@ -1,11 +1,11 @@
-const num = 0;
+const num = 2;
 const numeropar = (num % 2) === 0;
 console.log(numeropar);
 
 if(num === 0) {
     console.log('Número invalido');
 } else if (numeropar) {
-    console.log('o número é par')
+    console.log('o número é par');
 }
 else{
     console.log('O número é impar');
--- desafio02.js
+++ desafio02.js
@@ -11,4 +11,22 @@ Você terá 5 variáveis. Sendo elas:
 Imprima no console o valor que será gasto para realizar esta viagem.
 */
 
+const preco_etanol = 4.68;
+const preco_gasolina = 5.71;
+const km_por_litros = 10;
+const distancia_em_km = 100;
+const tipo_combustivel = 'gasolina';
+
+const consumoporlitro = distancia_em_km / km_por_litros;
+
+if (tipo_combustivel === 'etanol') {
+    console.log('etanol foi selecionado');
+    const valor_final = consumoporlitro * preco_etanol;
+    console.log(valor_final.toFixed(2));
+}
+else{
+    console.log('gasolina foi selecionada');
+    const valor_final = consumoporlitro * preco_gasolina;
+    console.log(valor_final.toFixed(2));
+}
 
