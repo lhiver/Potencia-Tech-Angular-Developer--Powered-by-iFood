7d70b12cab7c2aae6e0ee1ec8f99415378c5a7e4
Rodrigo Souza <rodrigosouza2k@outlook.com>
2023-04-30 22:41:32 -0300

    01

---
+++ funcoes2.js
@@ -0,0 +1,16 @@
+function escrevaMeuNome(nome){
+    console.log('Meu nome é ' + nome);
+}
+
+escrevaMeuNome('Rodrigo Souza');
+
+function Maioridade(idade){
+    if (idade >= 18){
+        console.log('voce é maior de idade');
+    } 
+    else{
+        console.log('voce é menor de idade');
+    }
+}
+
+Maioridade(28);
