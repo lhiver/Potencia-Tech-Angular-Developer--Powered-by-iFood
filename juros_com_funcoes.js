215edfc79c070987a1da60f3a63fd1e9fa0c2f1f
Rodrigo Souza <rodrigosouza2k@outlook.com>
2023-05-01 12:42:28 -0300

    01

--- juros_com_funcoes.js
+++ juros_com_funcoes.js
@@ -1,8 +1,5 @@
-
-
-const pagamento = 2;
+const pagamento = 3;
 const preco = 100;
-
 
 function com_desconto (valor,desconto){
     return (valor - (valor * (desconto /100)));
@@ -12,18 +9,17 @@ function com_juros (valor,juros){
     return (valor + (valor * (juros /100)));
 }
 
-
 if(pagamento === 1){
     console.log(com_desconto(preco,10));
 }
 else if(pagamento ===2){
-    console.log(com_juros(preco,10));
+    console.log(com_desconto(preco,15));
 }
 else if(pagamento === 3){
-        console.log('duas_vezes');
+        console.log(preco);
 }
 else if(pagamento === 4){
-    console.log('acimadeduas_vezes');
+    console.log(com_juros(preco,10));
 }
 
 
