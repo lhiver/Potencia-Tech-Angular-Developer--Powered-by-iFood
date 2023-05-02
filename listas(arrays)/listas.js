93873d1c2f1c22671164a457f11f96f4fa46adb6
Rodrigo Souza <rodrigosouza2k@outlook.com>
2023-05-01 22:04:17 -0300

    01

---
+++ listas(arrays)/listas.js
@@ -0,0 +1,17 @@
+const alunos = ['Rodrigo', 'Luana', 'Thamires'];
+
+/*add alunos */
+alunos.push('Renan');
+alunos[4] = ('Vinicius');
+
+console.log(alunos[3]);
+console.log(alunos);
+
+const notas = [];
+notas.push(8);
+notas.push(5);
+notas.push(5);
+notas.push(7);
+notas.push(6);
+
+console.log(notas.length);
