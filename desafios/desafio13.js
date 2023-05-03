d99ecaa5283858f3c22736cd13876f0dc4a7e71a
Rodrigo Souza <rodrigosouza2k@outlook.com>
2023-05-03 06:53:43 -0300

    01

--- desafios/desafio13.js
+++ desafios/desafio13.js
@@ -2,7 +2,8 @@
    Ex de lista de notas = [2, 7, 3, 8, 10, 4] */
    const notas = [2, 7, 3, 8, 10, 4];
    for (i=0; i<notas.length; i++ ){
-      if ([i]<){
+      if (notas[i]>9){
+         console.log(notas[i]);
 
       }
    }
