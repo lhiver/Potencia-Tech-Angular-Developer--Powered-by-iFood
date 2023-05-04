0eb5a30671e47fdbf1373a56f847f238728a6d86
Rodrigo Souza <rodrigosouza2k@outlook.com>
2023-05-03 23:34:29 -0300

    01

--- 02 - Desafios/desafio10.js
+++ 02 - Desafios/desafio10.js
@@ -1,5 +1,5 @@
 /*Faça um programa que dado uma lista de nomes imprima os nomes que começem com a letra "V". */
-const nomes = ['vando','felipe','vanessa','luana','Vanusa'];
+const nomes = ['vando','felipe','vanessa','luana','Vanusa, rita'];
 for (let i=0;i<nomes.length;i++){
     if ([0] ===v){
         console.log(nomes);
