2e3cf8b324111662ba2a1f412d848a5bbf9f7e52
Rodrigo Souza <rodrigosouza2k@outlook.com>
2023-05-09 22:03:38 -0300

    01

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,1 +1,8 @@
-console.log('sucesso');
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}0&limit=${limit}`
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.error(error))
