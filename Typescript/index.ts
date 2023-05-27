c33a9cc5027441a4e3bee01d908ef6a1c83ecb5c
Rodrigo Souza <rodrigosouza2k@outlook.com>
2023-05-27 12:33:16 -0300

    01

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -2,3 +2,12 @@ type heroi = {
     name: string;
     vulgo: string;
 };
+
+function printaObjeto(pessoa: heroi){
+    console.log(pessoa);
+}
+
+printaObjeto({
+    name: "bruce wayne",
+    vulgo: "batman",
+})
