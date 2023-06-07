7a553a94de8cfaef1869d40be24143416eb4505a
Rodrigo Souza <rodrigosouza2k@outlook.com>
2023-06-07 00:05:03 -0300

    01

--- 01 - Condicionais/condicionais.js
+++ 01 - Condicionais/condicionais.js
@@ -3,7 +3,7 @@ const numeropar = (num % 2) === 0;
 console.log(numeropar);
 
 if(num === 0) {
-    console.log('Número invalido');
+    console.log('O número é invalido');
 } else if (numeropar) {
     console.log('o número é par');
 }
