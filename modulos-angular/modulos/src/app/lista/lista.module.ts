d385e34535bc97d41427e28dd45e27ef97b1254f
Rodrigo Souza <rodrigosouza2k@outlook.com>
2023-06-07 09:53:43 -0300

    64

--- modulos-angular/modulos/src/app/lista/lista.module.ts
+++ modulos-angular/modulos/src/app/lista/lista.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
-import { ListViewComponent } from './list-view/list-view.component';
 
 
 
 @NgModule({
   declarations: [
+    InputComponent
+  ],
+  exports:[
     InputComponent,
-    ListViewComponent
   ],
   imports: [
     CommonModule
